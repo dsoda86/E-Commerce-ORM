484ddf7f18dba9f042f242fe21dc5932146441fd
dsoda86 <122141740+dsoda86@users.noreply.github.com>
2023-08-14 13:40:35 -0700

    Worked on code for models using given guidelines and info from seeds

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,27 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // from froduct-tag-seeds.js info (product_id, tag_id)
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,16 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // from tag-seeds.js ingo (tag_name)
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
